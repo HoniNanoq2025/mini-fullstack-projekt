d4ec80680e6425da3a77c3b4c231c48617b6f886
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-10-14 17:07:30 +0200

    create components files

--- frontend/src/pages/Recipes/Recipes.jsx
+++ frontend/src/pages/Recipes/Recipes.jsx
@@ -1,3 +1,6 @@
+import React, { useState, useEffect, useCallback } from "react";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import Pagination from "../../components/Pagination/Pagination";
 import styles from "./Recipes.module.css";
 
 export default function Recipes() {}
