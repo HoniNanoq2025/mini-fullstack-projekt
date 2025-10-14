f8d7395dd938d67f2e854ed1aec5253a66e261f1
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-10-14 13:56:20 +0200

    add image

--- backend/models/recipes.js
+++ backend/models/recipes.js
@@ -2,69 +2,79 @@
 const mongoose = require("mongoose");
 
 // Define Schema for recipes collection with validation
-const myRecipeSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: [true, "Title is required"], // Field must be filled out
-    minLength: [2, "Title must contain at least 2 characters"], // min. title length
-  },
-  description: {
-    type: String,
-    required: [true, "Description in required"],
-    minLength: [20, "Description must contain at least 20 characters"],
-  },
-  servings: {
-    type: Number,
-    required: [true, "Number of servings is required"],
-    min: [1, "Recipes must pride at least one serving"],
-  },
-  prepTimeMinutes: {
-    type: Number,
-    required: [true, "Preparation time in minutes is required"],
-    min: [1, "Recipes must have prep time of at least 1 minute"],
-  },
-  cookTimeMinutes: {
-    type: Number,
-    required: [true, "Cooking time in minutes is required"],
-    default: "None",
-  },
-  totalTimeMinutes: {
-    type: Number,
-    required: [
-      true,
-      "Total of Prep + cooking/freezing time in minutes is required",
-    ],
-    min: [1, "Recipes must have totalTime of at least 1 minute"],
-  },
-  ingredients: {
-    type: [String],
-    validate: {
-      validator: (arr) => arr.length > 0,
-      message: "You must add at least one ingredient",
+const myRecipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: [true, "Title is required"], // Field must be filled out
+      minLength: [2, "Title must contain at least 2 characters"], // min. title length
+    },
+    description: {
+      type: String,
+      required: [true, "Description in required"],
+      minLength: [20, "Description must contain at least 20 characters"],
+    },
+    servings: {
+      type: Number,
+      required: [true, "Number of servings is required"],
+      min: [1, "Recipes must pride at least one serving"],
+    },
+    prepTimeMinutes: {
+      type: Number,
+      required: [true, "Preparation time in minutes is required"],
+      min: [1, "Recipes must have prep time of at least 1 minute"],
+    },
+    cookTimeMinutes: {
+      type: Number,
+      required: [true, "Cooking time in minutes is required"],
+      default: "None",
+    },
+    totalTimeMinutes: {
+      type: Number,
+      required: [
+        true,
+        "Total of Prep + cooking/freezing time in minutes is required",
+      ],
+      min: [1, "Recipes must have totalTime of at least 1 minute"],
+    },
+    ingredients: {
+      type: [String],
+      validate: {
+        validator: (arr) => arr.length > 0,
+        message: "You must add at least one ingredient",
+      },
+    },
+    instructions: {
+      type: [String],
+      validate: {
+        validator: (arr) => arr.length > 0,
+        message: "You must add at least one instruction",
+      },
+    },
+    mealType: {
+      type: [String],
+      validate: {
+        validator: (arr) => arr.length > 0,
+        message: "You must at least one meal type (e.g. Lunch, Snack, Dinner)",
+      },
+    },
+    cuisine: {
+      type: String,
+      required: [
+        true,
+        "You need to indicate the type pf cuisine (e.g. Italian)",
+      ],
+      minLength: [4, "cuisine must contain at least 4 characters"],
+    },
+    difficulty: {
+      type: String,
+      required: [true, "You need to indicate the difficulty (e.g. Medium)"],
+      minLength: [4, "Difficulty must contain at least 4 characters"],
+    },
+    image: {
+      type: String,
+      default: "", // If no URL, then default setting is empty
     },
   },
-  instructions: {
-    type: [String],
-    validate: {
-      validator: (arr) => arr.length > 0,
-      message: "You must add at least one instruction",
-    },
-  },
-  mealType: {
-    type: [String],
-    validate: {
-      validator: (arr) => arr.length > 0,
-      message: "You must at least one meal type (e.g. Lunch, Snack, Dinner)",
-    },
-  },
-  cuisine: {
-    type: String,
-    required: [true, "You need to indicate the type pf cuisine (e.g. Italian)"],
-    minLength: [4, "cuisine must contain at least 4 characters"],
-  },
-  difficulty: {
-    type: String,
-    required: [true, "You need to indicate the difficulty (e.g. Medium)"],
-    minLength: [4, "Difficulty must contain at least 4 characters"],
-  },
-});
+  { timestamps: true }
+);
