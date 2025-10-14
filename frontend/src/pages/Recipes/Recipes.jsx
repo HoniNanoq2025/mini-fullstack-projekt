3132b65aca1c618d095c6ec21630bbef7eadab86
Hejdi Østergaard Nielsen <1000041081@mcdmstudent.dk>
2025-10-14 17:11:10 +0200

    add imports

--- frontend/src/pages/Recipes/Recipes.jsx
+++ frontend/src/pages/Recipes/Recipes.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import SearchBar from "../../components/SearchBar/SearchBar";
+import RecipeList from "../../components/RecipeList/RecipeList";
 import Pagination from "../../components/Pagination/Pagination";
 import styles from "./Recipes.module.css";
 
